41f5460982236b28f88ec6ed59ac372b3795144f
Ross Melville <rossmelville@hotmail.com>
2017-08-04 14:47:29 +0100

    Added Piano class and PianoTest class

---
+++ music_shop_management/Piano.java
@@ -0,0 +1,14 @@
+package music_shop_management;
+import behaviours.*;
+
+public class Piano implements Playable {
+
+  int keys;
+
+  public Piano(int keys) {
+    this.keys = keys;
+  }
+
+
+
+}
---
+++ music_shop_management_specs/PianoTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import music_shop_management.*;
+
+public class PianoTest {
+
+  Piano piano;
+
+  @Before
+  public void before() {
+    piano = new Piano(88);
+  }
+
+  
+
+}
