e3e9d1811b24459f061e168f05d6ace831540fd6
Ross Melville <rossmelville@hotmail.com>
2017-08-05 11:54:23 +0100

    Added Shop class and ShopTest class.

---
+++ music_shop_management/Shop.java
@@ -0,0 +1,17 @@
+package music_shop_management;
+import behaviours.*;
+import java.util.*;
+
+public class Shop {
+
+  private ArrayList<Sellable> stock;
+
+  public Shop() {
+    this.stock = new ArrayList<Sellable>();
+  }
+
+  
+
+
+
+}
