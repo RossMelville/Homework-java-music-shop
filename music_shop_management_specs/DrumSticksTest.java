dcf81dd7dba605b75da7e60189481f4b41b66afb
Ross Melville <rossmelville@hotmail.com>
2017-08-05 10:11:39 +0100

    Added tests to DrumSticksTest class to return instance variables of class DrumSticks.

---
+++ music_shop_management_specs/DrumSticksTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import music_shop_management.*;
+
+public class DrumSticksTest {
+
+  DrumSticks drumSticks;
+
+  @Before
+  public void before() {
+    drumSticks = new DrumSticks("Johnny Brooks", "Mapel", 3, 7.50);
+  }
+
+  @Test
+  public void canGetMake() {
+    assertEquals("Johnny Brooks", drumSticks.getMake());
+  }
+
+  @Test
+  public void canGetMaterial() {
+    assertEquals("Mapel", drumSticks.getMaterial());
+  }
+
+  @Test 
+  public void canGetBuyPrice() {
+    assertEquals(3.00, drumSticks.getBuyPrice(), 0.01);
+  }
+
+  @Test
+  public void canGetSellPrice() {
+    assertEquals(7.50, drumSticks.getSellPrice(), 0.01);
+  }
+
+  @Test
+  public void canGetMarkup() {
+    assertEquals(3.50, drumSticks.calculateMarkup());
+  }
+
+
+}
